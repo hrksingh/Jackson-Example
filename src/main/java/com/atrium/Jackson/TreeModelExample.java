ce838022acb6337397bf182a66eff32a7388c4d3
hrksingh <hrksingh17@gmail.com>
2020-07-26 21:35:53 +0530

    Removed unused imports

--- src/main/java/com/atrium/Jackson/TreeModelExample.java
+++ src/main/java/com/atrium/Jackson/TreeModelExample.java
@@ -2,7 +2,6 @@ package com.atrium.Jackson;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -16,36 +15,35 @@ public class TreeModelExample {
 
 		try {
 			JsonNode root = mapper.readTree(new File("e:\\staff.json"));
-			
+
 			// Get Name
-            String name = root.path("staff_name").asText();
-            System.out.println(name);
-           
-            //Get Age
-            Long id = root.path("age").asLong();
-            System.out.println(id);
-            
-            
-            //Get Salary
-            JsonNode salaryNode = root.path("salary");
-            if(!salaryNode.isMissingNode()) {
-            	System.out.println("2018 :"+salaryNode.path("2018").asText());
-            	System.out.println("2019 :"+salaryNode.path("2019").asText());
-            	System.out.println("2020 :"+salaryNode.path("2020").asText());
-            }
-            
-            // Get Skills
-            JsonNode skillNode = root.path("skills");
-            
-            if(skillNode.isArray()) {
-            	
-            	for(JsonNode node:skillNode) {
-            		System.out.println(node);
-            		
-            	}
-            	
-            }
-			
+			String name = root.path("staff_name").asText();
+			System.out.println(name);
+
+			// Get Age
+			Long id = root.path("age").asLong();
+			System.out.println(id);
+
+			// Get Salary
+			JsonNode salaryNode = root.path("salary");
+			if (!salaryNode.isMissingNode()) {
+				System.out.println("2018 :" + salaryNode.path("2018").asText());
+				System.out.println("2019 :" + salaryNode.path("2019").asText());
+				System.out.println("2020 :" + salaryNode.path("2020").asText());
+			}
+
+			// Get Skills
+			JsonNode skillNode = root.path("skills");
+
+			if (skillNode.isArray()) {
+
+				for (JsonNode node : skillNode) {
+					System.out.println(node);
+
+				}
+
+			}
+
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
