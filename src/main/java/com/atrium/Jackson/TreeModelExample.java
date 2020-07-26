3892b1ff4a0731ad411d5b326359cfa28e513ac3
hrksingh <hrksingh17@gmail.com>
2020-07-26 21:26:24 +0530

    Json Parsing Using TreeModel

---
+++ src/main/java/com/atrium/Jackson/TreeModelExample.java
@@ -0,0 +1,57 @@
+package com.atrium.Jackson;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TreeModelExample {
+
+	private static final ObjectMapper mapper = new ObjectMapper();
+
+	public static void main(String[] args) {
+
+		try {
+			JsonNode root = mapper.readTree(new File("e:\\staff.json"));
+			
+			// Get Name
+            String name = root.path("staff_name").asText();
+            System.out.println(name);
+           
+            //Get Age
+            Long id = root.path("age").asLong();
+            System.out.println(id);
+            
+            
+            //Get Salary
+            JsonNode salaryNode = root.path("salary");
+            if(!salaryNode.isMissingNode()) {
+            	System.out.println("2018 :"+salaryNode.path("2018").asText());
+            	System.out.println("2019 :"+salaryNode.path("2019").asText());
+            	System.out.println("2020 :"+salaryNode.path("2020").asText());
+            }
+            
+            // Get Skills
+            JsonNode skillNode = root.path("skills");
+            
+            if(skillNode.isArray()) {
+            	
+            	for(JsonNode node:skillNode) {
+            		System.out.println(node);
+            		
+            	}
+            	
+            }
+			
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
